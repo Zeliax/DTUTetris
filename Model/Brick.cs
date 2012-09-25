a32e61be851e3e6ce4503886463a19c053e068e0
Thomas Hammerbak <thomashammerbak@gmail.com>
2012-09-25 11:18:58 +0200

    Changed most files and imported other nescesary stuff

---
+++ Model/Brick.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Tetris.Model
+{
+    public class Brick : NotifyBase
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        
+        // Konstruktoren bruges i dette tilfælde til at sætte standard værdi for attributerne.
+        public Brick()
+        {
+            private int x;
+        public int X { get { return x; } set { x = value; NotifyPropertyChanged("X"); NotifyPropertyChanged("CanvasCenterX"); } }
+        private int y;
+        public int Y { get { return y; } set { y = value; NotifyPropertyChanged("Y"); NotifyPropertyChanged("CanvasCenterY"); } }
+        private int width;
+        public int Width { get { return width; } set { width = value; NotifyPropertyChanged("Width"); NotifyPropertyChanged("CenterX"); NotifyPropertyChanged("CanvasCenterX"); } }
+        private int height;
+        public int Height { get { return height; } set { height = value; NotifyPropertyChanged("Height"); NotifyPropertyChanged("CenterY"); NotifyPropertyChanged("CanvasCenterY"); } }
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Tetris.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace Tetris.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<Brick> Bricks { get; set; }
+        public MainViewModel()
+        {
+
+
+            Bricks = new ObservableCollection<Brick>();
+            Brick brick1 = new Brick();
+            brick1.X = 0;
+            brick1.Y = 0;
+
+            //Brick brick2 = new Brick();
+            //brick2.X = 100;
+            //brick2.Y = -550;
+
+            Bricks.Add(brick1);
+          //  Bricks.Add(brick2);
+
+            while(true) {
+                foreach (Brick child in Bricks) {
+                    child.Y++;
+                }
+                System.
+        
+        }
+    }
+}
